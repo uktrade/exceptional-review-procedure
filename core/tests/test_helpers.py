bd3602206900c41f4fa51759cc70c0faaa4669a7
richtier <rikatee@gmail.com>
2019-09-25 17:23:16 +0100

    Fix tests

--- core/tests/test_helpers.py
+++ core/tests/test_helpers.py
@@ -1,11 +1,24 @@
 import pytest
 
 from django.contrib.sessions.exceptions import SuspiciousSession
+from django.core.cache import cache
 
 from core import helpers
 
 
 def test_load_saved_submission_not_shared(rf):
+    cache.set('wizard_wizard_foo', {
+        helpers.CacheStorage.extra_data_key: {
+            helpers.CacheStorage.is_shared_key: False
+        }
+    })
+    request = rf.get('/')
+    with pytest.raises(SuspiciousSession):
+        helpers.load_saved_submission(request=request, key='foo')
+
+
+
+def test_load_saved_submission_not_exist(rf):
     request = rf.get('/')
     with pytest.raises(SuspiciousSession):
         helpers.load_saved_submission(request=request, key='foo')
